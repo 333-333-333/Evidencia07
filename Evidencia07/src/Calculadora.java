368d5e756d161d90df9d4ad9bc4cea3d2a0a0425
333-333-333 <104991957+333-333-333@users.noreply.github.com>
2022-10-08 22:25:48 -0300

    Ejercicio subido

---
+++ Evidencia07/src/Calculadora.java
@@ -0,0 +1,40 @@
+public class Calculadora {
+
+	private int n1;
+	private int n2;
+
+	public Calculadora() {
+		this.n1 = 0;
+		this.n2 = 0;
+	}
+
+	public Calculadora(int num1, int num2) {
+		this.n1 = num1;
+		this.n2 = num2;
+	}
+
+	public int sumar() {
+		return this.n1 + this.n2;
+	}
+
+	public int multiplicar() {
+		return this.n1 * this.n2;
+	}
+
+	/**
+	 * 
+	 * @param num1
+	 */
+	public void setN1(int num1) {
+		this.n1 = num1;
+	}
+
+	/**
+	 * 
+	 * @param num2
+	 */
+	public void setN2(int num2) {
+		this.n2 = num2;
+	}
+
+}
